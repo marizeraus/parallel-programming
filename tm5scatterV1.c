fe52f6e07ef41218b1b154eca16e671f9c7decf8
Mariana Suarez <msuarez@id.uff.br>
2022-05-09 12:49:23 -0300

    TM 5 - Find value in array using scatter and send/receive

---
+++ tm5scatterV1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define N 100000
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Input reading for process 0
+    int x;
+    int* buffer;
+    int randArray[N];
+    int send = 0;
+    double time;
+    if (rank == 0)
+    {
+
+        scanf("%d", &x);
+        //read X and now starts sending it to each process. 
+        for (int i=1; i < size; i ++) {
+            MPI_Send(&x, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        time = -MPI_Wtime();
+
+        for(int i=0;i<N;i++)
+            randArray[i]=rand()%100;   //Generate number between 0 to 99
+        send = N / size;
+
+        if (send * N < size)
+            send ++;
+    } else {
+        //all processess but master receive X. 
+    	MPI_Recv(&x, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //considering N is always divisible by size, may not be accurate. 
+    int finalSize = N/size;
+    buffer = (int *)malloc((finalSize) * sizeof(int));
+
+    MPI_Scatter(randArray, finalSize, MPI_INT, buffer, finalSize, MPI_INT,
+    0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    //received part from scatter and now starts looping through array to see if value is found. 
+    for (int i=0; i<finalSize; i++) {
+        if (buffer[i] == x) {
+            printf(" %d Encontrado no processo %d, na posição %d\n",buffer[i],rank,  i + rank * finalSize);
+        }
+    }
+
+    if(rank == 0){
+        printf("Time elapsed: %f", time + MPI_Wtime());
+    }
+    MPI_Finalize();
+    //free(buffer);
+
+    return 0;
+}
---
+++ tm5send_receiveV1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#define N 100000
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Input reading for process 0
+    int x;
+    int startPos = 0;
+    int finalPos = 0;
+    int buffer[N];
+    int randArray[N];
+    int send = 0;
+
+    double time;
+
+    if (rank == 0)
+    {
+        scanf("%d", &x);
+        time = -MPI_Wtime();
+        //read X and now starts sending it to each process. 
+        for (int i=1; i < size; i ++) {
+            MPI_Send(&x, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        for(int i=0;i<N;i++){
+            randArray[i]=rand()%100;   //Generate number between 0 to 99
+        }
+        send = N / size;
+        
+        finalPos = send;
+        for (int i=1; i < size; i ++) {
+            int pos, fin;
+            pos = send * i;
+            fin = pos + send;
+            //For each process, sending full array, starting position to analyze and final position. 
+            MPI_Send(randArray, N, MPI_INT, i, 0, MPI_COMM_WORLD);
+        	MPI_Send(&pos, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        	MPI_Send(&fin, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+        }
+    } else {
+    	MPI_Recv(&x, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(randArray, N, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    	MPI_Recv(&startPos, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    	MPI_Recv(&finalPos, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    for (int i=startPos; i<finalPos; i++) {
+        // look for X in the determined interval for this process. 
+        if (randArray[i] == x) {
+
+            printf(" %d Encontrado no processo %d, na posição %d\n",x, rank, i);
+        }
+    }
+    if(rank == 0){
+        printf("Time elapsed: %f", time + MPI_Wtime());
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
